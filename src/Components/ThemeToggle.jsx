c1ca938b819520435eae5d2ea99948cb1ac32f84
Ishani <ishani.nirala143@gmail.com>
2025-05-30 18:29:36 +0530

    Describe what you changed in ThemeToggle.jsx

--- src/Components/ThemeToggle.jsx
+++ src/Components/ThemeToggle.jsx
@@ -4,19 +4,25 @@ import {useEffect, useState} from "react";
 import {cn} from "@/lib/utils"
 
 export const ThemeToggle = () => {
-    const[isDarkMode, setIsDarkMode] = useState(false);
+    const[isDarkMode, setIsDarkMode] = useState(true);
 
-    useEffect(() => { 
-        const storedTheme = localStorage.getItem("theme")
-        if (storedTheme == "dark") {
-            setIsDarkMode(true);
-            document.documentElement.classList.add("dark");
-        }else{
-            localStorage.setItem("theme", "light");
-            setIsDarkMode(false);
+      useEffect(() => { 
+        const storedTheme = localStorage.getItem("theme");
+
+        if (storedTheme === "dark") {
+          setIsDarkMode(true);
+          document.documentElement.classList.add("dark");
+        } else if (storedTheme === "light") {
+          setIsDarkMode(false);
+          document.documentElement.classList.remove("dark");
+        } else {
+          // No theme stored yet, default to dark mode
+          localStorage.setItem("theme", "dark");
+          setIsDarkMode(true);
+          document.documentElement.classList.add("dark");
         }
-    }
-    , []);
+    }, []);
+
     
     const toggleTheme = () => {
         if(isDarkMode){
